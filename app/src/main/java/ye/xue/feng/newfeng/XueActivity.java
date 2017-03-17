cef5010fd7f62a4d874dbebc3024354e7e198181
houhonglei <houhonglei@bbktel.com>
2017-03-17 17:20:21 +0800

    ac master

--- app/src/main/java/ye/xue/feng/newfeng/XueActivity.java
+++ app/src/main/java/ye/xue/feng/newfeng/XueActivity.java
@@ -9,6 +9,8 @@ public class XueActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_xue);
+        String name="test";
         String email="klasdkjfl;j";
+        String wu="五分之1";
     }
 }
