8a372395e53ae3deacf7fed276207c062a020d1a
houhonglei <houhonglei@bbktel.com>
2017-03-17 16:34:44 +0800

    Initial commit

---
+++ app/src/main/java/ye/xue/feng/newfeng/XueActivity.java
@@ -0,0 +1,13 @@
+package ye.xue.feng.newfeng;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class XueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xue);
+    }
+}
